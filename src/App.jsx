c56522a85c67f3be575a535ed2e6f554e08a542f
Ignacio Barcelo <ignacio.barcelo@poliglota.org>
2023-02-08 20:15:58 -0300

    create Pet component in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,41 @@
-import React from "react";
 import { createRoot } from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me :)"),
-    React.createElement(Pet, {
-      animal: "Perro",
-      name: "Canela",
-      breed: "Kilterrier",
-    }),
-    React.createElement(Pet, {
-      animal: "Pato",
-      name: "Pata China",
-      breed: "Flor de Haba",
-    }),
-    React.createElement(Pet, {
-      animal: "Gato",
-      name: "Caqui",
-      breed: "Gato con Botas",
-    }),
-    React.createElement(Pet, {
-      animal: "Perro",
-      name: "Sombra",
-      breed: "Kilterrier",
-    })
-  );
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h1", {}, "Adopt Me :)"),
+  //   React.createElement(Pet, {
+  //     animal: "Perro",
+  //     name: "Canela",
+  //     breed: "Kilterrier",
+  //   }),
+  //   React.createElement(Pet, {
+  //     animal: "Pato",
+  //     name: "Pata China",
+  //     breed: "Flor de Haba",
+  //   }),
+  //   React.createElement(Pet, {
+  //     animal: "Gato",
+  //     name: "Caqui",
+  //     breed: "Gato con Botas",
+  //   }),
+  //   React.createElement(Pet, {
+  //     animal: "Perro",
+  //     name: "Sombra",
+  //     breed: "Kilterrier",
+  //   })
+  // );
+
+  <div>
+    <h1>Adopt Me :</h1>
+    <Pet name="canela" animal="perro" breed="kilterrier" />
+    <Pet name="sombra" animal="perro" breed="kilterrier" />
+    <Pet name="caqui" animal="gato" breed="gato con botas" />
+  </div>;
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
