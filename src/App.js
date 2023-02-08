ef0ea6187588f0ca9706c9feb55d6c224bb7893a
Ignacio Barcelo <ignacio.barcelo@poliglota.org>
2023-02-08 06:38:43 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt Me :)"),
+    React.createElement(Pet, {
+      animal: "Perro",
+      name: "Canela",
+      breed: "Kilterrier",
+    }),
+    React.createElement(Pet, {
+      animal: "Pato",
+      name: "Pata China",
+      breed: "Flor de Haba",
+    }),
+    React.createElement(Pet, {
+      animal: "Gato",
+      name: "Caqui",
+      breed: "Gato con Botas",
+    }),
+    React.createElement(Pet, {
+      animal: "Perro",
+      name: "Sombra",
+      breed: "Kilterrier",
+    })
+  );
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
