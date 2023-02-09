d9bc250fd852af467f2e23e1806afa18cf05ecb2
Ignacio Barcelo <ignacio.barcelo@poliglota.org>
2023-02-09 07:48:20 -0300

    fix bug in animal select

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,60 +1,65 @@
 import { useState } from "react";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
-const breeds = ["puma", "coipo"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
-  // es lo mismo que esto:
-  // const locationHook = useState("");
-  // const location = locationHook[0];
-  // const setLocation = locationHook[1];
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
-
+  const breeds = ["puma", "coipo", "huachimingo"];
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
           <input
-            onChange={(e) => setLocation(e.target.value)}
             id="location"
             value={location}
             placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
           />
         </label>
+
         <label htmlFor="animal">
           Animal
           <select
             id="animal"
-            value="{animal}"
+            value={animal}
             onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
               setAnimal(e.target.value);
               setBreed("");
             }}
           >
             <option />
             {ANIMALS.map((animal) => (
-              <option key={animal}>{animal}</option>
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
             ))}
           </select>
         </label>
+
         <label htmlFor="breed">
           Breed
           <select
+            disabled={!breeds.length}
             id="breed"
-            disabled={breeds.length === 0}
-            value="{breed}"
-            onChange={(e) => {
-              setBreed(e.target.value);
-            }}
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
           >
             <option />
             {breeds.map((breed) => (
-              <option key={breed}>{breed}</option>
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
             ))}
           </select>
         </label>
+
         <button>Submit</button>
       </form>
     </div>
