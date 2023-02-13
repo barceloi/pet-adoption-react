6888f08a2af72ec2aeb7dcc8beb23e7cc29f7d47
Ignacio Barcelo <ignacio.barcelo@poliglota.org>
2023-02-13 09:35:20 -0300

    install prettier tailwind plugin, adds styles to header and form

--- src/App.jsx
+++ src/App.jsx
@@ -18,21 +18,30 @@ const queryClient = new QueryClient({
 const App = () => {
   const adoptedPetHook = useState(null);
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AdoptedPetContext.Provider value={adoptedPetHook}>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <h1>Adopt Me :</h1>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-            <Route />
-          </Routes>
-        </AdoptedPetContext.Provider>
-      </QueryClientProvider>
-    </BrowserRouter>
+    <div
+      className=" m-0 p-0"
+      style={{
+        background: "url(https://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
+      }}
+    >
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <AdoptedPetContext.Provider value={adoptedPetHook}>
+            <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
+              <Link className="text-6xl text-white hover:text-gray-200" to="/">
+                Adopt Me!
+              </Link>
+            </header>
+            <h1>Adopt Me :</h1>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+              <Route />
+            </Routes>
+          </AdoptedPetContext.Provider>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </div>
   );
 };
 
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -20,8 +20,9 @@ const SearchParams = () => {
   const pets = results?.data?.pets ?? [];
 
   return (
-    <div className="search-params">
+    <div className="my-0 mx-auto w-11/12">
       <form
+        className="mb-10 flex flex-col items-center justify-center rounded-lg bg-gray-200 p-10 shadow-lg"
         onSubmit={(e) => {
           e.preventDefault();
           const formData = new FormData(e.target);
