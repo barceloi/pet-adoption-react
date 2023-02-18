3cbc7a0ddc7b4d54a40540574294e759b890908f
Ignacio Barcelo <ignacio.barcelo@poliglota.org>
2023-02-18 09:49:04 -0300

    changes details, carousel and pet context to ts

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,25 +1,20 @@
-import { lazy, useState, useContext } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { PetAPIResponse } from "./APIResponsesTypes";
 import { useQuery } from "@tanstack/react-query";
+import { useContext, useState } from "react";
 import AdoptedPetContext from "./AdoptedPetContext";
+import Modal from "./Modal";
 import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
-import { PetAPIResponse } from "./APIResponsesTypes";
-
-const Modal = lazy(() => import("./Modal"));
 
 const Details = () => {
+  const { id } = useParams();
   const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
-  // eslint-disable-next-line no-unused-vars
+  const results = useQuery < PetAPIResponse > (["details", id], fetchPet);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [_, setAdoptedPet] = useContext(AdoptedPetContext);
-  const { id } = useParams();
-  if (!id) {
-    throw new Error("why did you not give me an id. I wanted an id");
-  }
-
-  const results = useQuery(["details", id], fetchPet);
 
   if (results.isLoading) {
     return (
@@ -29,22 +24,24 @@ const Details = () => {
     );
   }
 
-  const pet = results.data.pets[0];
+  const pet = results?.data?.pets[0];
+
+  if (!pet) {
+    throw new Error("pet not found");
+  }
 
   return (
     <div className="details">
       <Carousel images={pet.images} />
       <div>
-        <h2>{pet.name}</h2>
-        <h3>
-          {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
-        </h3>
-        <button onClick={() => setShowModal(true)}>Adoptar {pet.name}</button>
+        <h1>{pet.name}</h1>
+        <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
         <p>{pet.description}</p>
         {showModal ? (
           <Modal>
             <div>
-              <h2>Quieres adoptar a {pet.name}?</h2>
+              <h1>Would you like to adopt {pet.name}?</h1>
               <div className="buttons">
                 <button
                   onClick={() => {
@@ -52,7 +49,7 @@ const Details = () => {
                     navigate("/");
                   }}
                 >
-                  Si
+                  Yes
                 </button>
                 <button onClick={() => setShowModal(false)}>No</button>
               </div>
@@ -64,12 +61,10 @@ const Details = () => {
   );
 };
 
-function DetailsErrorBoundary(props) {
+export default function DetailsErrorBoundary(props) {
   return (
     <ErrorBoundary>
       <Details {...props} />
     </ErrorBoundary>
   );
 }
-
-export default DetailsErrorBoundary;
